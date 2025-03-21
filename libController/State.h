a721f48bd5928af492c546a085f2076324780638
loumcnamee <mcnamee.lou@gmail.com>
2025-03-21 18:48:24 -0400

    Add plant power setting and set point temp to base state class

--- libController/State.h
+++ libController/State.h
@@ -8,11 +8,20 @@
 class IState {
 protected:
     std::string name_;
+    // input power to the system (heater or cooler)
+    // + = add heat, - = remove heat
+    float power_;
+
+    float setpoint_;
     
 public:
     IState(const std::string &name) : name_{name} {}
     virtual ~IState() = default;
     virtual const std::string& name() const = 0;
+    void setPower(float pwr) { power_ = pwr; }
+    float power() const { return power_; }
+    void setSetPoint(float setpoint) { setpoint_ = setpoint; }
+    float setPoint() const { return setpoint_; }
 };
 
 #endif // STATE_H
